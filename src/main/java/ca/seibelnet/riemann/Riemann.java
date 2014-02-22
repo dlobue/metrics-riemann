d05a402bd4bc0ab1755b3cf5fa839836073c711e
Dominic LoBue <dominic.lobue@gmail.com>
2014-02-21 17:20:14 -0800

    Use the riemann batch client class
    Use the included batch client class instead of rolling our own. The
    previous batching implementation sent all metrics gathered at once,
    while the batch client class has a configurable batch size. This will
    help in avoiding overloading the riemann server by sending it too many
    metrics at once.

--- src/main/java/ca/seibelnet/riemann/Riemann.java
+++ src/main/java/ca/seibelnet/riemann/Riemann.java
@@ -2,6 +2,8 @@ package ca.seibelnet.riemann;
 
 import com.aphyr.riemann.client.EventDSL;
 import com.aphyr.riemann.client.RiemannClient;
+import com.aphyr.riemann.client.AbstractRiemannClient;
+import com.aphyr.riemann.client.RiemannBatchClient;
 
 import java.io.Closeable;
 import java.io.IOException;
@@ -11,12 +13,16 @@ public class Riemann implements Closeable {
     String riemannHost;
     Integer riemannPort;
 
-    RiemannClient client;
+    AbstractRiemannClient client;
 
     public Riemann(String host, Integer port) throws IOException {
+        this(host, port, 10);
+    }
+    public Riemann(String host, Integer port, int batchSize) throws IOException {
         this.riemannHost = host;
         this.riemannPort = port;
-        this.client = RiemannClient.tcp(riemannHost, riemannPort);
+        this.client = new RiemannBatchClient(batchSize,
+                RiemannClient.tcp(riemannHost, riemannPort));
     }
 
     public void connect() throws IOException {
